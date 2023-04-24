2fa354e91b7fede4a6dd798e4d899285082dddbe
irpanch <irpanch@gmail.com>
2023-04-25 06:30:59 +0700

    try fasstr package (not done yet)

--- 02_code/tidy_data.R
+++ 02_code/tidy_data.R
@@ -14,9 +14,37 @@ View(rainfall_data)
 
 # coba package fasstr
 library(fasstr)
-rename(rainfall_data,c("Date" = "date", "Value" = "rainfall_mm" ))
-rainfall_data$rainfall_mm <- as.numeric(rainfall_data$rainfall_mm)
 
+tidyhydat::hy_version()
 
+calc_longterm_daily_stats(data = flow_data)
 
-calc_longterm_daily_stats(rainfall_data)
+plot_flow_data(station_number = "08NM116")
+
+calc_longterm_daily_stats(station_number = "08NM116")
+calc_longterm_daily_stats(station_number = c("08NM116", "08NM242"))
+
+plot_flow_data(station_number = c("08NM241", "08NM242"),
+               one_plot = TRUE) 
+
+screen_flow_data(station_number = "08NM116")
+
+plot_data_screening(station_number = "08NM116")
+
+plot_missing_dates(station_number = "08NM116")
+
+plot_longterm_daily_stats(station_number = "08NM116", 
+                          start_year = 1974,
+                          inner_percentiles = c(25,75),
+                          outer_percentiles = c(10,90)) 
+
+plot_longterm_monthly_stats(station_number = "08NM116", 
+                          start_year = 1974,
+                          inner_percentiles = c(25,75),
+                          outer_percentiles = c(10,90)) 
+
+plot_monthly_stats(station_number = "08NM116", 
+                   start_year = 1974)[1]
+
+plot_daily_stats(station_number = "08NM116", 
+                 start_year = 1974) 
